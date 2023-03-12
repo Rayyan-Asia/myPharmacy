cd189ff73883ff6e286ba5a89d87138019d28cb1
Musab Abuasi <abuasimusab@gmail.com>
2023-03-12 17:33:16 +0200

    Adding intro layout

--- app/src/main/java/com/example/mypharmacy/ui/MainActivity.java
+++ app/src/main/java/com/example/mypharmacy/ui/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.mypharmacy.ui;
 
 import android.content.Intent;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.mypharmacy.R;
+import com.example.mypharmacy.ui.intro.IntroActivity;
 import com.example.mypharmacy.ui.name.NameActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        Intent introIntent = new Intent(this, IntroActivity.class);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startActivity(introIntent);
+            }
+        }, 5000);
     }
 }
---
+++ app/src/main/java/com/example/mypharmacy/ui/intro/IntroActivity.java
@@ -0,0 +1,13 @@
+package com.example.mypharmacy.ui.intro;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.mypharmacy.R;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
