b856410161e548e3f0eb12ab55c244cc58090703
Musab Abuasi <abuasimusab@gmail.com>
2023-04-07 09:25:54 +0300

    adding empty medical record page

---
+++ app/src/main/java/com/example/mypharmacy/ui/medRecord/LabTestsFragment.java
@@ -0,0 +1,6 @@
+package com.example.mypharmacy.ui.medRecord;
+
+import androidx.fragment.app.Fragment;
+
+public class LabTestsFragment extends Fragment {
+}
--- app/src/main/java/com/example/mypharmacy/ui/medRecord/MedicalRecordFragment.java
+++ app/src/main/java/com/example/mypharmacy/ui/medRecord/MedicalRecordFragment.java
@@ -1,14 +1,21 @@
 package com.example.mypharmacy.ui.medRecord;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 import com.example.mypharmacy.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class MedicalRecordFragment extends Fragment {
 
+    private MyPagerAdapter mAdapter;
+    private ViewPager mViewPager;
     public MedicalRecordFragment() {
         // Required empty public constructor
     }
@@ -19,4 +26,15 @@ public class MedicalRecordFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_medical_records, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mAdapter = new MyPagerAdapter(getChildFragmentManager());
+        mViewPager = view.findViewById(R.id.view_pager);
+        mViewPager.setAdapter(mAdapter);
+
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
 }
