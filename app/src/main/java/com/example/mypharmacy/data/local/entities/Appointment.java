18dd01f8cddadb488b0866d238a5c3a59019ead4
Musab Abuasi <abuasimusab@gmail.com>
2023-04-09 22:09:24 +0300

    adding appointment entity

--- app/src/main/java/com/example/mypharmacy/data/local/entities/Appointment.java
+++ app/src/main/java/com/example/mypharmacy/data/local/entities/Appointment.java
@@ -1,5 +1,14 @@
 package com.example.mypharmacy.data.local.entities;
 
+import java.time.LocalDate;
+
 public class Appointment {
+    public int id;
+    public int doctorId;
+    public int personId;
+    public int prescriptionId;
+    public String symptoms;
+    public String diagnosis;
+    public LocalDate dateOfAppointment;
 
 }
