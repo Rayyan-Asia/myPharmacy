29abfa1d33f095fa5112ef3096362d7d556f8c52
Rayyan-Asia <RayyanTawfieg@Gmail.com>
2023-05-16 23:02:47 +0300

    Create recycler view for lab tests

---
+++ app/src/main/java/com/example/mypharmacy/ui/medRecord/labTest/LabTestAdapter.java
@@ -0,0 +1,2 @@
+package com.example.mypharmacy.ui.medRecord.labTest;public class LabTestAdapter {
+}
---
+++ app/src/main/java/com/example/mypharmacy/ui/medRecord/labTest/LabTestViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.mypharmacy.ui.medRecord.labTest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LabTestViewHolder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_test_view_holder);
+    }
+}
