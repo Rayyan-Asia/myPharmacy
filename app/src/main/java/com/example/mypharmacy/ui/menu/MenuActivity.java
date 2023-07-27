061cd7d1ed4e222187f572fd159912b341df2b3f
zaid-kh <z.kh2800@gmail.com>
2023-07-27 11:54:32 +0300

    refactor: removed notification icon

--- app/src/main/java/com/example/mypharmacy/ui/menu/MenuActivity.java
+++ app/src/main/java/com/example/mypharmacy/ui/menu/MenuActivity.java
@@ -16,78 +16,74 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuActivity extends AppCompatActivity {
 
-        private BottomNavigationView bottomNavigationView;
-        private Toolbar toolbar;
+    private BottomNavigationView bottomNavigationView;
+    private Toolbar toolbar;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_menu);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
 
-            bottomNavigationView = findViewById(R.id.bottom_navigation);
-            toolbar = findViewById(R.id.toolbar);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        toolbar = findViewById(R.id.toolbar);
 
-            setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
 
-            // Set the listener for the BottomNavigationView
-            bottomNavigationView.setOnItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) item -> {
-                switch (item.getItemId()) {
-                    case R.id.home_tab:
-                        // Switch to the Home fragment
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_container, new HomeFragment())
-                                .commit();
-                        return true;
-                    case R.id.medical_record_tab:
-                        // Switch to the Medical Record fragment
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_container, new MedicalRecordFragment())
-                                .commit();
-                        return true;
-                    case R.id.medication_reminder_tab:
-                        // Switch to the Medication Reminder fragment
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_container, new MedicationReminderFragment())
-                                .commit();
-                        return true;
-                    case R.id.family_tab:
-                        // Switch to the Family fragment
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_container, new FamilyFragment())
-                                .commit();
-                        return true;
-                    case R.id.menstrual_calendar_tab:
-                        // Switch to the Menstrual Calendar fragment
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_container, new MenstrualCalendarFragment())
-                                .commit();
-                        return true;
-                    default:
-                        return false;
-                }
-            });
-
-            // Set the Home tab as the default tab
-            bottomNavigationView.setSelectedItemId(R.id.home_tab);
-        }
-
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-            return true;
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
+        // Set the listener for the BottomNavigationView
+        bottomNavigationView.setOnItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) item -> {
             switch (item.getItemId()) {
-                case R.id.notification_button:
-                    // Handle the notification button click
-                    // ...
+                case R.id.home_tab:
+                    // Switch to the Home fragment
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, new HomeFragment())
+                            .commit();
+                    return true;
+                case R.id.medical_record_tab:
+                    // Switch to the Medical Record fragment
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, new MedicalRecordFragment())
+                            .commit();
+                    return true;
+                case R.id.medication_reminder_tab:
+                    // Switch to the Medication Reminder fragment
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, new MedicationReminderFragment())
+                            .commit();
+                    return true;
+                case R.id.family_tab:
+                    // Switch to the Family fragment
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, new FamilyFragment())
+                            .commit();
+                    return true;
+                case R.id.menstrual_calendar_tab:
+                    // Switch to the Menstrual Calendar fragment
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, new MenstrualCalendarFragment())
+                            .commit();
                     return true;
                 default:
-                    return super.onOptionsItemSelected(item);
+                    return false;
             }
+        });
+
+        // Set the Home tab as the default tab
+        bottomNavigationView.setSelectedItemId(R.id.home_tab);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
+}
 
 
